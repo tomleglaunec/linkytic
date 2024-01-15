a5df211611ea6b9cfc89e965cbabf855eed556fd
Tom Benezech <tom.leglaunec@gmail.com>
2024-01-15 12:13:26 +0100

    fix: regex typo and string element sum

--- custom_components/linkytic/parser.py
+++ custom_components/linkytic/parser.py
@@ -1,17 +1,15 @@
 from abc import ABC
 import re
-from const import (
-    DATASET_HISTORIC,
-    DATASET_STANDARD,
-    FRAME_ENCODING
-)
+from const import DATASET_HISTORIC, DATASET_STANDARD, FRAME_ENCODING
+
 
 class Dataset(ABC):
     """Object to represent an universal dataset.
     Datasets are the fields of a frame and contains a tag-data pair (and enventually corresponding timestamp).
     Raw bytes (ascii encoded) shall be passed to the constructor, the class will check formatting and verify checksum.
     """
-    __slots__ = ('_tag', '_data', '_timestamp')
+
+    __slots__ = ("_tag", "_data", "_timestamp")
 
     _PATTERN: re.Pattern
     _tag: str
@@ -22,27 +20,27 @@ class Dataset(ABC):
         try:
             str_dataset = raw_dataset.decode(FRAME_ENCODING)
         except UnicodeDecodeError:
-            raise Exception
-        
+            raise MalformedDatasetError(raw_dataset)
+
         m = self._PATTERN.match(str_dataset)
         if not m:
             # No match = malformed dataset
-            raise Exception
+            raise MalformedDatasetError(raw_dataset)
 
-        self._tag = m.group('tag')
-        self._data = m.group('data')
-        self._timestamp = m.group('timestamp')
+        self._tag = m.group("tag")
+        self._data = m.group("data")
+        self._timestamp = m.group("timestamp")
 
-        checksum = ord(m.group('checksum'))
+        checksum = ord(m.group("checksum"))
 
-        checked_zone = m.group('checked')
+        checked_zone = m.group("checked")
 
         if (c := self._compute_checksum(checked_zone)) != checksum:
             raise InvalidChecksumError(raw_dataset, c, checksum)
 
     def _compute_checksum(self, checked) -> int:
         """Returns the checksum"""
-        S1 = sum(checked)
+        S1 = sum(ord(c) for c in checked)
         return (S1 & 0x3F) + 0x20
 
     def __repr__(self) -> str:
@@ -51,35 +49,43 @@ class Dataset(ABC):
     @property
     def tag(self) -> str:
         return self._tag
-    
+
     @property
     def data(self) -> str:
         return self._data
-    
+
     @property
     def timestamp(self) -> str | None:
         return self._timestamp
-    
+
 
 class HistoricDataset(Dataset):
     """Dataset implementing the historic format"""
+
     _PATTERN = re.compile(DATASET_HISTORIC, re.X)
 
 
 class StandardDataset(Dataset):
     """Dataset implementing the standard format"""
+
     _PATTERN = re.compile(DATASET_STANDARD, re.X)
 
 
 class TICParser(ABC):
     pass
 
+
 class StandardTICParser(TICParser):
     pass
+
 
 class HistoricTICParser(TICParser):
     pass
 
 
 class InvalidChecksumError(Exception):
+    pass
+
+
+class MalformedDatasetError(Exception):
     pass
